0b1d95146156fdc01bcd27ecdb9fa99bab98acb7
GreggtheDev <greggthedev@gmail.com>
2024-08-02 16:54:21 -0400

    initial commit

--- src/main/java/org/example/Solution1.java
+++ src/main/java/org/example/Solution1.java
@@ -1,2 +1,10 @@
-package org.example;public class Solution1 {
+// Define the function sleepIn which determines if we can sleep in based on two conditions
+public boolean sleepIn(boolean weekday, boolean vacation) {
+
+    // Return true if it is not a weekday or if we are on vacation
+    // !weekday: This checks if it is NOT a weekday. If weekday is true, !weekday will be false and vice versa.
+    // || vacation: This checks if we are on vacation. If either condition is true, the result will be true.
+    // return !weekday || vacation;
 }
+
+
